0901e18d4167c9918330a2b2d18932b093123fff
Artem Sokovets <relix@mail.ru>
2018-02-04 14:54:45 +0300

    init owner framework for read properties

---
+++ src/main/java/io/github/artsok/properties/ReRunConfig.java
@@ -0,0 +1,13 @@
+package io.github.artsok.properties;
+
+import org.aeonbits.owner.Config;
+
+/**
+ * Config for Owner Framework
+ *
+ * @author Artem Sokovets
+ */
+public class ReRunConfig implements Config {
+
+
+}
